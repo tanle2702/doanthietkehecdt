e68d487da25464cbab271dc082685e3b22198904
Tan Le <letrongtan2702@gmail.com>
2021-03-14 19:36:21 +0700

    finishing all modules and implement to main robot module, not tested

--- improcessing/MainRobot.py
+++ improcessing/MainRobot.py
@@ -1,7 +1,20 @@
 import CameraModule
-import LaneDetectionModule
-import MotorModule
+from LaneDetectionModule import getLaneCurve
+from MotorModule import Motor
+import BatteryModule
 import cv2
+from gpiozero import CPUTemperature
+
+#motor setup
+
+motor = Motor(24,23,17)
+
+def main():
+    img = CameraModule.getImg()
+    curveVal = getLaneCurve(img, display=1) 
+    sens = 1.3
+    motor.steer(curveVal)
 
 if __name__ == '__main__':
-    print("oke")
+    motor.move()
+    main()
